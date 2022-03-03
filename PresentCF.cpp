782e6608db1e117a6fd19b9f98291167865b2d62
Anuj Jain <anujjain8044@gmail.com>
2022-03-03 22:10:03 +0530

    cpcodes

---
+++ PresentCF.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pr;
+
+ll ar = LLONG_MAX;
+ll mod = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n+1];
+    int prr[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        prr[arr[i]]=i;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<prr[i]<<" ";
+    }
+    
+}
+int main()
+{
+    int t=1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
