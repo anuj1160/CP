28db1b0c00213a536ea6f969bf78c455ae2bb668
Anuj Jain <anujjain8044@gmail.com>
2022-04-22 21:09:06 +0530

    1100 rating question codeforces

---
+++ B_Sifid_and_Strange_Subsequences.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pr;
+
+ll ar = LLONG_MAX;
+ll mod = 1e9 + 7;
+void solve()
+{
+    int n;
+    cin >> n;
+    ll arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<ll> v;
+    ll mini = 1e18;
+    ll mini2 = 1e15;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= 0)
+        {
+            v.push_back(arr[i]);
+        }
+    }
+    if(v.size()==0)
+    {
+        cout<<"1"<<endl;
+        return ;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+        {
+            if (arr[i] < mini)
+            {
+                mini = arr[i];
+            }
+        }
+    }
+    sort(v.begin(), v.end());
+    vector<ll> v3;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        v3.push_back(abs(v[i] - v[i + 1]));
+    }
+    for (int i = 0; i < v3.size(); i++)
+    {
+        if (v3[i] < mini2)
+        {
+            mini2 = v3[i];
+        }
+    }
+    if (mini <= mini2)
+    {
+        cout << v.size() + 1 << endl;
+    }
+    else
+    {
+        cout << v.size() << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
