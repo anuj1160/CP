ca5d22eef6a64813f5405d0583ebeda6f502d501
Anuj Jain <anujjain8044@gmail.com>
2022-05-05 12:02:39 +0530

    1100 rating question

---
+++ C_Ping-pong.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pr;
+
+ll ar = LLONG_MAX;
+ll mod = 1e9 + 7;
+bool cmp(int a, int b)
+{
+    return b < a;
+}
+
+void solve()
+{
+    ll x,y;
+    cin>>x>>y;
+    cout<<x-1<<" "<<y<<"\n";
+
+
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
