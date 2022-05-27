194a53f59483e82767d39b69c72a24c22f48ef32
Anuj Jain <anujjain8044@gmail.com>
2022-05-27 16:27:52 +0530

    my template for cp

---
+++ Mytemplate.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pr;
+typedef vector<long long> vll;
+#define pb push_back
+#define ho(a) a.begin(), a.end()
+ll aj = LLONG_MAX;
+ll mod = 1e9 + 7;
+bool cmp(pr a, pr b)
+{
+    return a.second < b.second;
+}
+ll binpow3(ll a, ll b)
+{
+    ll res = 1;
+    a = a % mod;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+
+void solve()
+{
+   
+}
+
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
